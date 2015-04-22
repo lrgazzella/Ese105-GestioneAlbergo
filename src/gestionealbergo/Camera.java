240c105c95df10aa498bc08b8938a5148ff97b46
Lorenzo <lrgazzella@gmail.com>
2015-04-22 22:01:27 +0200

    Implementata classe Camera

--- src/gestionealbergo/Camera.java
+++ src/gestionealbergo/Camera.java
@@ -12,6 +12,7 @@ abstract public class  Camera {
 		this.numero = numero;
 		prenotazioni = new Vector<Prenotazione>(1,1);
 	}
+	
 	public String getNumero() {
 		return numero;
 	}
@@ -22,9 +23,27 @@ abstract public class  Camera {
 	public void addPrenotazione(Prenotazione prenotazione){
 		this.prenotazioni.addElement(prenotazione);
 	}
-	
 	public boolean isDisponibile(LocalDateTime dal, LocalDateTime al){
+		boolean value = false;
+		for(Prenotazione q : prenotazioni) {
+			int numeroNotti = q.getNumeroNotti();
+			LocalDateTime arrivo = q.getArrivoIl();
+
+			int anno = arrivo.getYear();
+			int mese = arrivo.getMonthValue();
+			int giorno = arrivo.getDayOfMonth();	
+			
+			LocalDateTime partenza = LocalDateTime.of(anno, mese, giorno+numeroNotti, 12, 00); //da migliorare
+			
+			if( (partenza.isBefore(dal)!= false) && (arrivo.isAfter(al) != false) ){
+				value = true;
+			}else{
+				value = false;
+			}
+				
+		}
 		
+		return value;
 	}
 	
 	
